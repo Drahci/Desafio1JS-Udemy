b67915e1b29dd340ccd6ca1e6610b4ffd0d46d7c
Richard <richard.ricciard@gmail.com>
2024-05-17 17:10:28 -0300

    Feat/desafio completo

---
+++ script.js
@@ -0,0 +1,1 @@
+document.getElementById("text").innerHTML = "Esse tipo de abordagem é essencial para criar interfaces de usuáriointerativas e responsivas, melhorando significativamente a experiência do usuário."
